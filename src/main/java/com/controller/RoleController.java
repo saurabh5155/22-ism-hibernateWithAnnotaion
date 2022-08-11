6466554349312070520a4f0ba3c260826ce0be28
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-11 19:58:14 +0530

    GetAllRoles useing List<?> findAll()

--- src/main/java/com/controller/RoleController.java
+++ src/main/java/com/controller/RoleController.java
@@ -1,7 +1,10 @@
 package com.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,11 @@ public class RoleController {
 	public ResponseEntity<?> addRole(RoleBean role){
 		roleDao.save(role);
 		return ResponseEntity.ok().body(role);
+	}
+	
+	@GetMapping("/role")
+	public ResponseEntity<?> getAllRoles(){
+		List<RoleBean> roles = roleDao.findAll();
+		return ResponseEntity.ok().body(roles);
 	}
 }
--- src/main/java/com/dao/RoleDao.java
+++ src/main/java/com/dao/RoleDao.java
@@ -1,5 +1,7 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.bean.RoleBean;
 
 @Repository
 public interface RoleDao extends CrudRepository<RoleBean, Integer>{
-
+	List<RoleBean> findAll();
 }
