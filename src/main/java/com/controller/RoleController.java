9853861fe561a75648c081557f93b737ea79af07
saurabh5155 <s.a.patel5155@gmail.com>
2022-08-11 20:46:49 +0530

    DeleteRoleById useing deleteById()

--- src/main/java/com/controller/RoleController.java
+++ src/main/java/com/controller/RoleController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,5 +38,11 @@ public class RoleController {
 		Optional<RoleBean> roleById = roleDao.findById(roleId);
 		RoleBean role = roleById.get();
 		return ResponseEntity.ok().body(role);
+	}
+	
+	@DeleteMapping("/role/{roleId}")
+	public ResponseEntity<?> deleteRoleById(@PathVariable("roleId") int roleId){
+		roleDao.deleteById(roleId);
+		return ResponseEntity.ok(HttpStatus.ACCEPTED);
 	}
 }
